5272eb471646c0d260537edf4e25150c371c3a2a
Matheus Gomes <matheuskshn@gmail.com>
2025-05-13 10:54:20 -0300

    refactor: reorganize imports and improve config variable assignment in NetPing plugin

--- netbox_netping/__init__.py
+++ netbox_netping/__init__.py
@@ -1,7 +1,10 @@
-# ---------------------------
-# netbox_netping/__init__.py
-# ---------------------------
-"""Top‑level package for NetPing plugin."""
+"""Top-level package for NetPing plugin."""
 __version__ = "0.3.0"
 
+from .config import NetPingConfig
+
+# NetBox procura uma variável de módulo chamada `config`
+config = NetPingConfig
+
+# Opcional (legado); não atrapalha em NetBox 4.x
 default_app_config = "netbox_netping.config.NetPingConfig"
