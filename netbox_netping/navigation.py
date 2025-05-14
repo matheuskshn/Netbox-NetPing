d63a5b57fc369e1a032d96b65852c794904be81d
Matheus Gomes <39656344+matheuskshn@users.noreply.github.com>
2025-05-14 11:51:29 -0300

    refactor: simplify navigation menu by removing unused buttons and reversing URL resolution

--- netbox_netping/navigation.py
+++ netbox_netping/navigation.py
@@ -1,17 +1,10 @@
 # netbox_netping/navigation.py
-from netbox.plugins import PluginMenuItem, PluginMenuButton
-from django.urls import reverse
+from netbox.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link=reverse("plugins:netbox_netping:job"),     # view gerada pelo Job
+        link="plugins:netbox_netping:job",     # view gerada pelo Job
         link_text="Ping prefixes / IPs",
-        buttons=(
-            PluginMenuButton(
-                link="plugins:netbox_netping:job",
-                title="Run now",
-                icon_class="mdi mdi-play",
-            ),
-        ),
+        buttons=(),
     ),
-)
+),
