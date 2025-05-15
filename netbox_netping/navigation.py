ac0027cddeeaede7ea81788c0559cee707e5e851
Matheus Gomes <39656344+matheuskshn@users.noreply.github.com>
2025-05-15 02:55:44 -0300

    refactor: substituir URL absoluta por link dinâmico usando reverse para o botão de execução do Ping Job

--- netbox_netping/navigation.py
+++ netbox_netping/navigation.py
@@ -1,22 +1,20 @@
 # netbox_netping/navigation.py
+from django.urls import reverse
 from netbox.plugins import PluginMenuItem, PluginMenuButton
 
-# Caminho absoluto até o formulário “Run Job”
-PING_JOB_URL = "/extras/jobs/enqueue/netbox_netping.jobs.PingJob/"
-
-# 1) Cria o botão (é um objeto, não uma tupla)
 button_ping_job = PluginMenuButton(
-    link=PING_JOB_URL,
+    link=lambda _: reverse(
+        "extras:job_enqueue",                # rota que exibe o formulário
+        kwargs={"job_class": "netbox_netping.jobs.PingJob"},
+    ),
     title="Run Ping Job",
     icon_class="mdi mdi-play-circle-outline",
 )
 
-# 2) Atribui o botão dentro de uma tupla ao menu
 menu_items = (
     PluginMenuItem(
         link="plugins:netbox_netping:home",
         link_text="Home",
-        permissions=[],          # visível a qualquer usuário autenticado
-        buttons=(button_ping_job,),   # ← note a vírgula final
+        buttons=(button_ping_job,),
     ),
 )
