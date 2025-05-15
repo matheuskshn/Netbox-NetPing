1c8a3467e20ae3672acf1cfc7e8190db824fe8e1
Matheus Gomes <39656344+matheuskshn@users.noreply.github.com>
2025-05-15 02:46:39 -0300

    feat: adicionar botão de menu para executar o Ping Job no NetBox

--- netbox_netping/navigation.py
+++ netbox_netping/navigation.py
@@ -1,10 +1,22 @@
 # netbox_netping/navigation.py
-from netbox.plugins import PluginMenuItem
+from netbox.plugins import PluginMenuItem, PluginMenuButton
 
 menu_items = (
     PluginMenuItem(
         link="plugins:netbox_netping:home",
         link_text="Home",
         permissions=[],           # visível para qualquer usuário autenticado
+    ),
+)
+
+menu_buttons = (
+    PluginMenuButton(
+        link=lambda _: reverse(
+            "extras:job_run",  # view core do NetBox
+            kwargs={"job_class": "netbox_netping.jobs.PingJob"},
+        ),
+        title="Run Ping Job",
+        icon_class="mdi mdi-play-circle-outline",
+        color="green",
     ),
 )
