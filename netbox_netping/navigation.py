7b3e534a2aec6e9cf3f06dcc039eec43575551b4
Matheus Gomes <39656344+matheuskshn@users.noreply.github.com>
2025-05-15 02:53:47 -0300

    refactor: remover código desnecessário e reorganizar a criação do botão de execução do Ping Job

--- netbox_netping/navigation.py
+++ netbox_netping/navigation.py
@@ -1,23 +1,22 @@
 # netbox_netping/navigation.py
 from netbox.plugins import PluginMenuItem, PluginMenuButton
-from django.urls import reverse
 
+# Caminho absoluto até o formulário “Run Job”
+PING_JOB_URL = "/extras/jobs/enqueue/netbox_netping.jobs.PingJob/"
+
+# 1) Cria o botão (é um objeto, não uma tupla)
+button_ping_job = PluginMenuButton(
+    link=PING_JOB_URL,
+    title="Run Ping Job",
+    icon_class="mdi mdi-play-circle-outline",
+)
+
+# 2) Atribui o botão dentro de uma tupla ao menu
 menu_items = (
     PluginMenuItem(
         link="plugins:netbox_netping:home",
         link_text="Home",
-        permissions=[],           # visível para qualquer usuário autenticado
-    ),
-)
-
-menu_buttons = (
-    PluginMenuButton(
-        link=lambda _: reverse(
-            "extras:job_run",  # view core do NetBox
-            kwargs={"job_class": "netbox_netping.jobs.PingJob"},
-        ),
-        title="Run Ping Job",
-        icon_class="mdi mdi-play-circle-outline",
-        color="green",
+        permissions=[],          # visível a qualquer usuário autenticado
+        buttons=(button_ping_job,),   # ← note a vírgula final
     ),
 )
